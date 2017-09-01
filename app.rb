bbb88d52527c7a43f04699eb07494b3a87aea78a
Guillermo Salazar <guillermos_2007@yahoo.com>
2017-09-01 11:40:19 -0400

    initial commit

---
+++ app.rb
@@ -0,0 +1,11 @@
+require 'sinatra'
+require './pet'
+require './pet_parser'
+# require './pet_serializer'
+pets = ['{"id": 0 ,"name": "doggie"}','{"id": 1, "name": "puppie"}']
+set :environment, :development
+get '/pets' do
+end
+post '/pet' do
+
+end
---
+++ pet.rb
@@ -0,0 +1,7 @@
+class Pet
+  attr_accessor :id, :name
+  def initialize(args)
+    @id = args["id"]
+    @name = args["name"]
+  end
+end
